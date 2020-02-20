65443b2edc695c7d071c69a79c33a91373977ad0
Lewis Bell <lewisbell999@gmail.com>
2020-02-20 18:09:28 +0000

    Update formula to newest release

--- temple.rb
+++ temple.rb
@@ -1,9 +1,9 @@
 class Temple < Formula
   desc "A Software Framework for automatically generating microservices from a high level DSL"
-  version "0.001"
-  homepage "https://templeeight.github.io/"
-  url "https://github.com/TempleEight/temple/releases/download/v0.01/temple-latest"
-  sha256 "7b4c97734bdf7a2b0c45a499115e094e866830c427becaa60f777fb0cfe3dc1d"
+  version "0.1.0"
+  homepage "https://templeeight.github.io/temple-docs/"
+  url "https://github.com/TempleEight/temple/releases/download/v0.1.0/temple-latest"
+  sha256 "875e66d382f256daea838f3e5b5d1c3392c272016c19974e38c34548ad8b9792"
 
 
   def install
