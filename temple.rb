51f19be6c467e8bee09e6e7e27a0fd78ddcf66cf
Lewis Bell <lewisbell999@gmail.com>
2020-04-29 10:36:46 +0100

    Release v1.0.0

--- temple.rb
+++ temple.rb
@@ -1,9 +1,9 @@
 class Temple < Formula
   desc "A framework for generating microservice infrastructure for platform-based systems"
-  version "0.1.0"
+  version "1.0.0"
   homepage "https://templeeight.github.io/temple-docs/"
-  url "https://github.com/TempleEight/temple/releases/download/v0.1.0/temple-latest"
-  sha256 "875e66d382f256daea838f3e5b5d1c3392c272016c19974e38c34548ad8b9792"
+  url "https://github.com/TempleEight/temple/releases/download/v1.0.0/temple-latest"
+  sha256 "25d4df714f9916665cb687b50a298eb75f1a8d256c83932d9ad04c4d0018637c"
 
 
   def install
